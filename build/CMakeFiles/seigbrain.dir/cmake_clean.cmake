91761d103903da591206bfcc42199a7bb49d79c5
chanleyuu <hanley-c3@ulster.ac.uk>
2020-01-20 13:29:06 +0000

    Begun designing

---
+++ build/CMakeFiles/seigbrain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/seigbrain.dir/main.cpp.o"
+  "seigbrain"
+  "seigbrain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/seigbrain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- nueron.h
+++ nueron.h
@@ -1,6 +1,19 @@
 #pragma once
 
+#include <vector>
 
 class neuron {
+private:
+    std::vector<int> condition_; //conditions
+    bool fire_;
+public: 
+    neuron(); //default constructor 
     
+    bool isfireing();
+    
+    bool getfire() const;
+    void setfire(bool fire);
+    
+    std::vector<int> getcondition() const;
+    void setcondition(std::vector<int> condition); 
 };
