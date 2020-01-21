d0b9a5386128b112a4550a663e5fd8562a129211
chanleyuu <hanley-c3@ulster.ac.uk>
2020-01-21 09:13:43 +0000

    renamed some files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include "neuron.h"
+
+
 int main(int argc, char **argv) {
     std::cout << "Hello, Time!" << std::endl;
     return 0;
--- neuron.h
+++ neuron.h
@@ -6,14 +6,20 @@ class neuron {
 private:
     std::vector<int> condition_; //conditions
     bool fire_;
+    int number_;
 public: 
     neuron(); //default constructor 
     
-    bool isfireing();
+    neuron(int num, std::vector<int> cond); //custom constructor 
+    
+    bool isfireing(std::vector<int> check);
     
     bool getfire() const;
     void setfire(bool fire);
     
     std::vector<int> getcondition() const;
     void setcondition(std::vector<int> condition); 
+    
+    int getnumber() const;
+    void setnumber(int num);
 };
