37b650bbd1b030c72aedfc17814fd808ce60c644
chanleyuu <hanley-c3@ulster.ac.uk>
2020-01-30 10:55:25 +0000

    Yes

--- network.cpp
+++ network.cpp
@@ -13,10 +13,32 @@
 
 network::network()
 {
+    for (int i = 0; i < 10000; i++) {
+        neuron nuer( i, 0.0, 0.0);
+        baselayer.addneuron(nuer);
+    }
+        
+    for (int i = 0; i < 32; i++) {
+        neuron nuer( i, 0.0, 0.0);
+        hiddenlayer1.addneuron(nuer);
+    }
     
+    for (int i = 0; i < 32; i++) {
+        neuron nuer( i, 0.0, 0.0);
+        hiddenlayer2.addneuron(nuer);
+    }
+    
+    for (int i = 0; i < 8; i++) {
+        neuron nuer( i, 0.0, 0.0);
+        outputlayer.addneuron(nuer);
+    }
 }
 
 double network::think()
 {
+    hiddenlayer1.conntectneurons(baselayer);
+    hiddenlayer2.conntectneurons(hiddenlayer1);
+    outputlayer.conntectneurons(hiddenlayer2);
+    
     
 }
